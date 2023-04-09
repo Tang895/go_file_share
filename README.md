75d26e3473e201b5a17cb82575ed3c91385505e2
ianTang <tangyijjj@outlook.com>
2023-04-09 16:36:58 +0800

    add interface selected options

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ go写的一个局域网文件分享软件，直接生成二维码，手机扫码
 - 使用方法
 
 ```bash
-send xxx.txt
+sendfile xxx.txt
 ```
 ![image](https://github.com/Tang895/go_file_share/blob/34b6213f6c9a4a6e75c4e1739b60aa50dfe37ade/show_res.png)
---
+++ sendfile.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+
+	qrcode "github.com/skip2/go-qrcode"
+)
+
+var filename string
+var filesize int
+var file_Path string
+
+func sendfile(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintln(w, "hello world!")
+	header := w.Header()
+	header.Add("Content-Type", "application/octet-stream")
+	header.Add("Content-Disposition", "attachment;filename="+filename)
+	file, _ := ioutil.ReadFile(file_Path)
+
+	header.Add("Content-Length", strconv.Itoa(filesize))
+	w.Write(file)
+	//fmt.Println(w.Header())
+}
+func main() {
+	//sys_Info := runtime.GOOS //获取计算机系统
+
+	interfaceaddrs, _ := net.InterfaceAddrs()
+	ip := ""
+	var ips []string
+	ipItor := 1
+	for i := range interfaceaddrs {
+		ip = interfaceaddrs[i].String()
+
+		ips = append(ips, ip)
+		fmt.Println("[", ipItor, "]", ": ", ip)
+		ipItor += 1
+		// if ip[:8] == "192.168." {
+		// 	ip = ip[:len(ip)-3] //去掉掩码
+		// 	break
+		// }
+	}
+	//fmt.Println(ips)
+	fmt.Print("Select your interface, 选择网口->")
+	var interfaceSelect int
+	fmt.Scanln(&interfaceSelect)
+	ip = ips[interfaceSelect-1]
+	file_Path = os.Args[1]
+	file_Info, _ := os.Stat(file_Path)
+	filesize = int(file_Info.Size())
+	filename = file_Info.Name()
+	//加md5
+	filename_Byte := md5.Sum([]byte(file_Path))
+	filename_MD5 := fmt.Sprintf("%x", filename_Byte)
+
+	http.HandleFunc("/"+filename_MD5, sendfile)
+
+	dl_URL := "http://" + ip + ":8000/" + filename_MD5
+
+	q, _ := qrcode.New(dl_URL, qrcode.Low)
+	art := q.ToString(false)
+	fmt.Println(art) //输出二维码
+	fmt.Println("file:" + file_Path)
+	//fmt.Println(filename)
+	fmt.Println(dl_URL)
+	http.ListenAndServe(":8000", nil)
+}
